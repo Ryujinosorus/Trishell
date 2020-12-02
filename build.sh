0e5709520d3379c9f55f65117230f023e73b35b3
Ryujinosorus <56671819+Ryujinosorus@users.noreply.github.com>
2020-12-02 19:06:51 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #INSTALL COMMAND
-sudo cp src/trishell /usr/bin/
+sudo cp src/trishell.sh /usr/bin/trishell
 sudo chmod 755 /usr/bin/trishell
 
 #ADD MAN PAGES
-sudo cp trishell /usr/local/man/man1/trishell.1
+sudo cp man_trishell /usr/local/man/man1/trishell.1
 sudo gzip /usr/local/man/man1/trishell.1
